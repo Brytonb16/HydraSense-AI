a73abad0bf6fc95afa14a396688b6de17e727a20
Brytonb16 <brookebryton@gmail.com>
2025-07-15 13:44:31 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,3 +12,19 @@ export default function Splash() {
     </div>
   );
 }
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function SplashRedirect() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/login');
+  }, [router]);
+
+  return (
+    <div>
+      <p>Redirecting to Login...</p>
+    </div>
+  );
+}
